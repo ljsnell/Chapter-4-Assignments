172ca825b51c7d4908cae9d7e4f120bc57415469
ljsnell <ljsnell7@gmail.com>
2022-08-06 13:10:45 -0500

    Solution
    Solution

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,5 +1,15 @@
 public class FormLetterWriter {
+    static String businessLetter = " Thank you for your recent order";
     public static void main(String[] args) {
+        displaySalutation("Doe");
+        displaySalutation("Jill", "Doe");
+    }
 
+    private static void displaySalutation(String lastName) {
+        System.out.println("Dear Mr. or Ms. " + lastName + businessLetter);
+    }
+
+    private  static  void displaySalutation(String firstName, String lastName) {
+        System.out.println("Dear " + firstName + " " + lastName + businessLetter);
     }
 }
